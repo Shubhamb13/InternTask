5ee1bbcddc89b40be12ad614e6842d93cefa3a48
SHUBHAM  BHADARAGE <136561695+Shubhamb13@users.noreply.github.com>
2024-03-26 18:35:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+
+import Lighthome from "./Intern/Lighthome"
+import Lightcom from "./Intern/Lightcom"
+
+
+
+const App = () => {
+  return (
+
+    <div className='App'>
+      
+      <Lighthome/>
+      <Lightcom/>
+      
+    </div>
+   
+  )
+}
+
+export default App
